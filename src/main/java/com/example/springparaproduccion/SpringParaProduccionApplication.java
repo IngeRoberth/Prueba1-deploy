28070606af96f0b6d8da6ebfebd5eefaf7d9b27a
Roarco <sebuscaroberth@gmail.com>
2022-09-16 21:45:59 -0500

    Preparando Despliegue

---
+++ src/main/java/com/example/springparaproduccion/SpringParaProduccionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springparaproduccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParaProduccionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringParaProduccionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springparaproduccion/SpringParaProduccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springparaproduccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringParaProduccionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
